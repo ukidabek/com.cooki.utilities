bc7eb58bbca2edb58f93f81857674267a7009a9b
Łukasz Dąbek <ukidabek@gmail.com>
2025-12-28 19:30:22 +0100

    [Code] Collection interface updated.

--- Runtime/General/ICollection.cs
+++ Runtime/General/ICollection.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Utilities.General
 {
-    public interface ICollection<T> : IDictionary<Key, T>, ITickableElement where T : IKeyableElement
+    public interface ICollection<T> : IReadOnlyDictionary<Key, T>, ITickableElement where T : IKeyableElement
     {
         void Initialize();
     }
